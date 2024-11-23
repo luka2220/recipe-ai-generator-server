f915f6295dee86d50c230ea28864b173c80c03b1
Luka Piplica <42144047+luka2220@users.noreply.github.com>
2024-11-22 20:21:11 -0500

    client communication working with CORS

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,33 @@
 import express, { Express, Response, Request } from 'express';
+import cors from 'cors';
 
 const app: Express = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
+
+app.use((req, res, next) => {
+    console.log('Incoming request from origin:', req.get('Origin'));
+    next();
+});
+
+const corsOptions = (req: Request, res: Response, next: Function) => {
+    const allowedOrigins = ['http://localhost:5173'];
+    const origin = req.get('Origin') ?? '';
+
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    } else {
+        res.status(403).json({ message: 'Forbidden: CORS Policy' });
+    }
+};
+
+app.use(corsOptions);
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome').status(200);
+    res.json({ message: 'Hello from server!!!!' }).status(200);
+    console.log('Sent data to client!');
 });
 
 app.listen(PORT, () => {
